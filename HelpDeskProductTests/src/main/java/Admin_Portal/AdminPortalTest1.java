1b761bbf8748f8ca0005f822c55f56460bcaafb5
ajaysuresh582 <ajaysuresh82@gmail.com>
2024-05-27 22:00:09 -0700

    AdminPortalTest1

--- HelpDeskProductTests/src/main/java/Admin_Portal/AdminPortalTest1.java
+++ HelpDeskProductTests/src/main/java/Admin_Portal/AdminPortalTest1.java
@@ -10,25 +10,18 @@ import CommonTests.CommonTests1;
 
   @Test
   public void Scenario1() {
-	  
 	   String Methodname = new Object(){}.getClass().getEnclosingMethod().getName(); 
-	
 		try {
-			
 			String Classname = getClass().getSimpleName();
             System.out.println("start of method : " + Classname);
             loginasAdmin();
             TestCase1();
             TestCase3();
-				
 		} 
-		
 		catch (Throwable e) {
 			e.printStackTrace();
 			Assert.fail("Error in "+ Methodname +" : "+ e.getMessage());
 		}
-		
-	
   } 
   
  }
